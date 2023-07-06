c5ada98da70f10d28d7b27aed1063cd92bb31fd1
Kendi Anyika <kendi.anyika@gmail.com>
2023-07-06 12:48:07 +0300

    Setting up side Navigation

--- app/src/main/java/com/example/thrivematch/ui/HomeActivity.kt
+++ app/src/main/java/com/example/thrivematch/ui/HomeActivity.kt
@@ -2,10 +2,15 @@ package com.example.thrivematch.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import androidx.core.view.GravityCompat
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.thrivematch.R
 import com.example.thrivematch.databinding.ActivityHomeBinding
 
@@ -22,21 +27,39 @@ class HomeActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
-
         val navController = findNavController(R.id.fragmentContainerView2)
         val appBarConfiguration = AppBarConfiguration(topLevelDestinationIds = setOf())
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            if (destination.id == R.id.homeFragment|| destination.id == R.id.likedFragment || destination.id == R.id.matchedFragment) {
-                binding!!.bottomNavigationView.visibility = View.VISIBLE
-                binding.toolbarActivityHome.visibility = View.VISIBLE
-            } else {
-                binding!!.bottomNavigationView.visibility = View.GONE
-                binding.toolbarActivityHome.visibility = View.GONE
+//        setupActionBarWithNavController(navController, appBarConfiguration)
 
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment, R.id.likedFragment, R.id.matchedFragment -> {
+                    binding!!.bottomNavigationView.visibility = View.VISIBLE
+                    binding.toolbarActivityHome.visibility = View.VISIBLE
+                }
+                R.id.myAccountFragment, R.id.documentUploadFragment, R.id.subscriptionFragment -> {
+                    binding.toolbarActivityHome.visibility = View.VISIBLE
+                    binding!!.bottomNavigationView.visibility = View.GONE
+                }
+                else -> {
+                    binding!!.bottomNavigationView.visibility = View.GONE
+                    binding.toolbarActivityHome.visibility = View.GONE
+
+                }
             }
         }
-
         setupWithNavController(binding!!.bottomNavigationView, navController)
+        setupWithNavController(binding.sideNav, navController)
+
+        // Open Side Navigation
+        binding.sideDrawerIcon.setOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+
+
+
 
     }
 
